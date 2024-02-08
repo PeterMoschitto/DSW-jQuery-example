7e87a8f22f9a4ba9ae29cfa0207b15b700da9da3
PeterMoschitto <pdmoschitto@gmail.com>
2024-02-08 14:17:30 -0800

    CSS

--- myscript.js
+++ myscript.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
 		$("img").toggle();
 	});
 	$("h1").click(function() {
-		$("p").css({"color":"red", "background-color": "yellow"});
+		$("p").css({"color":"purple", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
+	});
+	
+// 	$("div").animate({left: '250px'});
+	$("#p1").click(function(){
+		$(".disappear").hide();
+	});
+	$(".disappear").dblclick(function(){
+		$("#img1").css({"border":"10px inset red","display": "block", "margin": "auto"});
+		$("#img1").fadeOut("slow");
 	});
 });
