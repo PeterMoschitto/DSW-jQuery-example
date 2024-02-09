0c8cd0ee79be2232ff15bf50c443fa1a7154307b
PeterMoschitto <pdmoschitto@gmail.com>
2024-02-09 13:53:10 -0800

    Started project

--- myscript.js
+++ myscript.js
@@ -1,18 +1,44 @@
 $(document).ready(function() {
-	$("#imagetoggler").click(function(){
-		$("img").toggle();
-	});
-	$("h1").click(function() {
-		$("p").css({"color":"purple", "background-color": "yellow"});
-		$("img").css("border","25px solid black");
+	$("#button1").click(function(){
+		$(".WrongAns1").fadeOut();
+		
 	});
 	
-// 	$("div").animate({left: '250px'});
-	$("#p1").click(function(){
-		$(".disappear").hide();
+	$("#button2").click(function(){
+		$(".WrongAns2").fadeOut();
+		
 	});
-	$(".disappear").dblclick(function(){
-		$("#img1").css({"border":"10px inset red","display": "block", "margin": "auto"});
-		$("#img1").fadeOut("slow");
+	
+	$("#button3").click(function(){
+		$(".WrongAns3").fadeOut();
+		
 	});
+	
+	
+	
+	
+	
+	
+	
+	// $("#imagetoggler").click(function(){
+// 		$("img").toggle();
+// 	});
+// 	$("h1").click(function() {
+// 		$("p").css({"color":"purple", "background-color": "yellow"});
+// 		$("img").css("border","25px solid black");
+// 	});
+// 	
+// // 	$("div").animate({left: '250px'});
+// 	$("#p1").click(function(){
+// 		$(".disappear").hide();
+// 	});
+// 	
+// 	$(".disappear").dblclick(function(){
+// 		$("#img1").css({"border":"10px inset red","display": "block", "margin": "auto"});
+// 		$("#img1").fadeOut("slow");
+// 	});
+// 	
+// 	$("#p2").click(function(){
+// 		$(".slideUp(3000)");
+// 	});
 });
