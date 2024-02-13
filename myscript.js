167fc8ef2133eddcd043a24692094e535d14a02e
PeterMoschitto <pdmoschitto@gmail.com>
2024-02-13 13:57:15 -0800

    Added questions

--- myscript.js
+++ myscript.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
 	$("#button1").click(function(){
-		$(".WrongAns1").fadeOut();
+		$(".WrongAns1").fadeOut(2000);
 		
 	});
 	
 	$("#button2").click(function(){
-		$(".WrongAns2").fadeOut();
+		$(".WrongAns2").fadeOut(2000);
 		
 	});
 	
 	$("#button3").click(function(){
-		$(".WrongAns3").fadeOut();
+		$(".WrongAns3").fadeOut(2000);
 		
 	});
 	
 	$("#button1").dblclick(function(){
-		$(".WrongAns1").fadeIn();
+		$(".WrongAns1").fadeIn(2000);
 		
 	});
 	
 	$("#button2").dblclick(function(){
-		$(".WrongAns2").fadeIn();
+		$(".WrongAns2").fadeIn(2000);
 		
 	});
 	
 	$("#button3").dblclick(function(){
-		$(".WrongAns3").fadeIn();
+		$(".WrongAns3").fadeIn(2000);
 		
 	});
+	
+	// $("#flag").click(function(){
+// 		$("#flag").css("transform", "rotate(20deg)");
+// 	});
 });
 	
 	
